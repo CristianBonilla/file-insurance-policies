9629aa217d017633fd37672f79cf5d55c2d493db
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-01 03:45:56 -0500

    :boom: Policy entity date fields is updated and added

--- Vehicle.InsurancePolicies.Domain.Entities/PolicyEntity.cs
+++ Vehicle.InsurancePolicies.Domain.Entities/PolicyEntity.cs
@@ -7,7 +7,9 @@ namespace Vehicle.InsurancePolicies.Domain.Entities
     public string VehicleId { get; set; } = string.Empty;
     public string PlanName { get; set; } = string.Empty;
     public decimal MaxValueCovered { get; set; }
-    public DateTime WasTaken { get; set; }
+    public DateTime TakenDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
     public CustomerEntity? Customer { get; set; }
     public VehicleEntity? Vehicle { get; set; }
     public ICollection<CoverageEntity> Coverages { get; set; } = new HashSet<CoverageEntity>();
