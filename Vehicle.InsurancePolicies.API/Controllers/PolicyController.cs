a778c8898f0860caf1b85107414ab1053d8190c8
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-14 00:50:32 -0500

    :construction: Policy controller API created and add policy is allowed

---
+++ Vehicle.InsurancePolicies.API/Controllers/PolicyController.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Vehicle.InsurancePolicies.Contracts.DTO.Policy;
+using Vehicle.InsurancePolicies.Contracts.Services;
+using Vehicle.InsurancePolicies.Domain.Entities;
+using Vehicle.InsurancePolicies.Domain.Entities.Transfers;
+
+namespace Vehicle.InsurancePolicies.API.Controllers
+{
+  [Route("api/v{version:apiVersion}/[controller]")]
+  [ApiController]
+  [ApiVersion("1.0")]
+  [Produces("application/json")]
+  public class PolicyController : ControllerBase
+  {
+    readonly IMapper _mapper;
+    readonly IPolicyService _service;
+
+    public PolicyController(IMapper mapper, IPolicyService service)
+    {
+      _mapper = mapper;
+      _service = service;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PolicyResponse))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> AddPolicy([FromBody] PolicyRequest policyRequest)
+    {
+      PolicyEntity policy = _mapper.Map<PolicyEntity>(policyRequest);
+      PolicyTransfer policyTransfer = await _service.AddPolicy(policy, policyRequest.StartDate, policyRequest.EndDate);
+      PolicyResponse policyResponse = _mapper.Map<PolicyResponse>(policyTransfer);
+
+      return Ok(policyResponse);
+    }
+  }
+}
