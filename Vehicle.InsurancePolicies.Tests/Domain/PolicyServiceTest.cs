174399fac84428c5dba3d47c0834edb477d45dd3
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2023-06-21 01:17:03 -0500

    :construction: Mock vehicle repository and unit tests config in progress

--- Vehicle.InsurancePolicies.Tests/Domain/PolicyServiceTest.cs
+++ Vehicle.InsurancePolicies.Tests/Domain/PolicyServiceTest.cs
@@ -1,4 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Vehicle.InsurancePolicies.Contracts.Services;
+using Vehicle.InsurancePolicies.Domain.Context;
+using Vehicle.InsurancePolicies.Domain.Repositories;
+using Vehicle.InsurancePolicies.Domain.Services;
 
 namespace Vehicle.InsurancePolicies.Tests.Domain
 {
@@ -6,11 +11,24 @@ namespace Vehicle.InsurancePolicies.Tests.Domain
   public class PolicyServiceTest
   {
     readonly IServiceCollection _serviceCollection = Startup.Instance.ServiceCollection;
+    IPolicyService _policyService;
+    readonly Mock<VehicleInsurancePoliciesContext> _mockContext = new();
+    readonly Mock<IVehicleInsurancePoliciesRepositoryContext> _mockRepositoryContext = new();
 
     [SetUp]
     public void SetUp()
     {
+      _serviceCollection.AddTransient(typeof(VehicleInsurancePoliciesContext), _ => _mockContext.Object);
+      _serviceCollection.AddTransient(typeof(IVehicleInsurancePoliciesRepositoryContext), _ => _mockRepositoryContext.Object);
+      _serviceCollection.AddTransient<IPolicyService, PolicyService>();
+      _policyService = _serviceCollection.BuildServiceProvider()
+        .CreateScope()
+        .ServiceProvider.GetRequiredService<IPolicyService>();
+    }
 
+    [Test]
+    public void Should_Add()
+    {
     }
   }
 }
---
+++ Vehicle.InsurancePolicies.Tests/Mocks/MockVehicleRepository.cs
@@ -0,0 +1,52 @@
+using System.Linq.Expressions;
+using MongoDB.Bson;
+using Moq;
+using Vehicle.InsurancePolicies.Domain.Entities;
+using Vehicle.InsurancePolicies.Domain.Repositories;
+
+namespace Vehicle.InsurancePolicies.Tests.Mocks
+{
+  class MockVehicleRepository
+  {
+    static IEnumerable<VehicleEntity> _vehicles => new VehicleEntity[]
+    {
+      new()
+      {
+        VehicleId = new ObjectId("647825af2c728e9a9974643a"),
+        Plate = "AAA-123",
+        Model = "Nissan",
+        HasInspection = true
+      },
+      new()
+      {
+        VehicleId = new ObjectId("647825af2c728e9a9974643b"),
+        Plate = "BBB-123",
+        Model = "Toyota",
+        HasInspection = false
+      },
+      new()
+      {
+        VehicleId = new ObjectId("647825af2c728e9a9974643c"),
+        Plate = "CCC-123",
+        Model = "Volkswagen",
+        HasInspection = true
+      }
+    };
+
+    static readonly IQueryable<VehicleEntity> _vehiclesQuery = _vehicles.AsQueryable();
+
+    public static Mock<IVehicleRepository> GetMock()
+    {
+      var mockVehicleRepository = new Mock<IVehicleRepository>();
+      mockVehicleRepository.Setup(expression => expression.Get()).Returns(() => _vehicles);
+      mockVehicleRepository.Setup(expression => expression.Exists(It.IsAny<Expression<Func<VehicleEntity, bool>>>()))
+        .Returns<Expression<Func<VehicleEntity, bool>>>(expression => _vehiclesQuery.Any(expression));
+      mockVehicleRepository.Setup(expression => expression.Find(It.IsAny<ObjectId>()))
+        .Returns<ObjectId>(vehicleId => _vehiclesQuery.FirstOrDefault(vehicle => vehicle.VehicleId == vehicleId));
+      mockVehicleRepository.Setup(expression => expression.Find(It.IsAny<Expression<Func<VehicleEntity, bool>>>()))
+        .Returns<Expression<Func<VehicleEntity, bool>>>(expression => _vehiclesQuery.FirstOrDefault(expression));
+
+      return mockVehicleRepository;
+    }
+  }
+}
